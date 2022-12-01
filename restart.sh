e578d3bd48527c834a6efb89efe9f7107e6aa7d1
root <ptah0414@naver.com>
2022-12-01 11:52:15 +0900

    tf

---
+++ restart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+terraform destroy -auto-approve && \
+terraform init && \
+terraform plan && \
+terraform apply -auto-approve && \
+aws eks --region $(terraform output -raw region) update-kubeconfig \
+    --name $(terraform output -raw cluster_name) 
---
+++ terraform.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 3.4.0"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.2.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.12.1"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0.1"
+    }
+  }
+
+  #required_version = "~> 1.3"
+}
+

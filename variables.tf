5c6a786d9fb9baedde9ddfe38f9ec04d3085198c
Lee Gunwoo <42131472+ptah0414@users.noreply.github.com>
2022-12-01 13:39:20 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,10 +3,3 @@ variable "region" {
   type        = string
   default     = "ap-northeast-2"
 }
-
-variable "db_password" {
-  default = "test1234"
-  description = "RDS root user password"
-  #sensitive   = true
-}
-
